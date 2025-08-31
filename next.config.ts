4329da1025bb1ab1be6f2df61ebe39642c8c2923
Raave Aires <dev@raave.me>
2025-08-30 23:23:06 -0300

    fix: adiciona pacote '@react-pdf/renderer' nas configurações experimentais

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     globalNotFound: true,
+    serverComponentsExternalPackages: ['@react-pdf/renderer']
   },
 };
 
